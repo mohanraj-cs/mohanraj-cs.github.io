167126957e05f97334d567dbf3c5d8721a2deef4
Mohan Raj <mohanraj746007@gmail.com>
2023-09-21 22:16:13 -0700

    delete all old

--- contexts/viewport/index.tsx
+++
@@ -1,6 +0,0 @@
-import useViewportContextState from "contexts/viewport/useViewportContextState";
-import contextFactory from "contexts/contextFactory";
-
-const { Provider, useContext } = contextFactory(useViewportContextState);
-
-export { Provider as ViewportProvider, useContext as useViewport };
